7b283d72ca1257ec80b14cc623ace02fd10f0e2c
yann <clementyann@free.fr>
2020-08-09 01:58:50 +0200

    mise a jour admin

--- src/Controller/Admin/PredictionsCrudController.php
+++ src/Controller/Admin/PredictionsCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Predictions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
 class PredictionsCrudController extends AbstractCrudController
 {
@@ -16,6 +19,12 @@ class PredictionsCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            AssociationField::new('Game', 'Match'),
+            AssociationField::new('User', 'Utilisateur'),
+            DateTimeField::new('CreatedAt', 'Date'),
+            IntegerField::new('HomeResult', 'Résultat Home'),
+            IntegerField::new('VisitorResult', 'Résultat visitor'),
+            AssociationField::new('Predict', 'Victoire'),
 
         ];
     }
