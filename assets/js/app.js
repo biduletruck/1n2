89c7b9d31ae801fa0eec9c7b90c666b63a60d6d3
yann <clementyann@free.fr>
2020-08-08 15:18:08 +0200

    Ajout Font Awesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,9 @@ import '../css/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
 $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
--- src/Security/CustomAuthAuthenticator.php
+++ src/Security/CustomAuthAuthenticator.php
@@ -96,8 +96,10 @@ class CustomAuthAuthenticator extends AbstractFormLoginAuthenticator implements 
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
+        }else{
+            return new RedirectResponse($this->urlGenerator->generate('matches_day'));
         }
-        return new RedirectResponse($this->urlGenerator->generate('matches_day'));
+
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
  //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
